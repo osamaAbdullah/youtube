6f8823ae8e16158fd7e28d1c3fe6dc5ba91d8169
Osama Akre <osama3akre@gmail.com>
2018-04-10 11:15:37 +0300

    searvhing is done successfully

---
+++ resources/views/layouts/partials/_video_result.blade.php
@@ -0,0 +1,20 @@
+<div class="row">
+    <div class="col-sm-5">
+        <a href="{{ url('videos/' . $video->uid ) }}">
+            <img src="{{ url('images/' . 'default.png' . '/view') }}" alt="{{ $video->title }} thumbnail" class="img-responsive">
+        </a>
+    </div>
+    <div class="col-sm-7">
+        <a href="{{ url('videos/' . $video->uid ) }}">{{ $video->title }}</a>
+        @if($video->description)
+            <p>{{ $video->description }}</p>
+        @else
+            <p>No description</p>
+        @endif
+        <ul class="list-inline">
+            <li><a href="{{ url('channels/' . $video->channel->slug ) }}">{{ $video->channel->name }}</a></li>
+            <li>{{ $video->created_at->diffForHumans() }}</li>
+            <li>{{ $video->viewCount() . '' . str_plural('view', $video->viewCount()) }}</li>
+        </ul>
+    </div>
+</div>
